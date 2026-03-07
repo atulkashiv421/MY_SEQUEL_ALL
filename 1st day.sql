3f7e884f904b564d519b806d210f78fcec2a9ec0
Atul <ddk801973@gmail.com>
2026-03-07 19:07:57 +0530

    Add files via upload

---
+++ 1st day.sql
@@ -0,0 +1,11 @@
+select *from professional;
+ALTER TABLE professional ADD COLUMN IS_ACTIVE BOOLEAN DEFAULT TRUE;
+ALTER TABLE professional DROP COLUMN IS_ACTIVE;
+select *from professional;
+select*from professional;
+alter table professional modify column Name varchar(100) after id ;
+select *from professional;
+alter table professional drop column Name1 ;
+select*from prodessionsl;
+
+
