e7c7b9a8ba1ac373259486ecdebe4f4116938104
Atul <ddk801973@gmail.com>
2026-03-16 21:35:23 +0530

    Add files via upload

---
+++ 8th day(set 1).sql
@@ -0,0 +1,13 @@
+INSERT INTO adresses (user_id,pincode,city) VALUES
+(1,'110001','Delhi'),
+(2,'122001','Gurgaon'),
+(3,'160001','Chandigarh'),
+(4,'201301','Noida'),
+(5,'226001','Lucknow'),
+(6,'302001','Jaipur'),
+(7,'380001','Ahmedabad'),
+(8,'400001','Mumbai'),
+(9,'560001','Bangalore'),
+(10,'700001','Kolkata');
+select*from users;
+select*from adresses;
